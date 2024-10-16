9e59d2bb606b1c9df3dba9dc0b3af58f9c2fbcdc
SHANUWA-MD <shanuwamdv@gmail.com>
2024-10-17 00:20:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+SESSION_ID: process.env.SESSION_ID || "wSMXXQIC#V_BzAz51-1-afSfCnYNoYbqgsiNA-m64314Q8vvco_4",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
