a9dd2091bb2246a69dfaab5ab68dcb5ca469b57d
SHANUWA-MD <shanuwamdv@gmail.com>
2024-10-16 21:43:02 +0530

    Add files via upload

---
+++ plugins/alive.js
@@ -0,0 +1,56 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require('os')
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    react: "⛺",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let desc = `
+*Hi 👉👈* ${pushname}
+
+╔════════════════◊
+║✧ *ᴜᴘᴛɪᴍᴇ :* _${runtime(process.uptime())}_
+║✧ *ʀᴀᴍ ᴜꜱᴀɢᴇ :*  _${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB_
+║✧ *ʜᴏꜱᴛ ɴᴀᴍᴇ :* _${os.hostname()}_
+║✧ *ᴏᴡɴᴇʀ :* _ꜱʜᴀɴᴜᴡᴀ ɪᴅ_
+╚════════════════◊
+
+*Shanuwage Wada Thama Ithin 👨‍💻💥*
+
+╔════════════════◊
+║ _*YOUTUBE LINK*_
+║ https://www.youtube.com/@shanuwatech
+╚════════════════◊
+╔════════════════◊
+║ _*SHANUWA MD GITHUB REPO*_
+║ https://github.com/HaCkr-EPZI-public/CuduNona 
+╚════════════════◊
+╔════════════════◊
+║ _*WHATSAPP GROUP*_
+║ https://chat.whatsapp.com/Br3OsfVzPMcF2qPcCWLAw5
+╚════════════════◊
+╔════════════════◊
+║ _*WHATSAPP CHANNEL*_
+║ https://whatsapp.com/channel/0029ValzLhUBqbrFa8tMPg3U
+╚════════════════◊
+
+> *©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱʜᴀɴᴜᴡᴀ ɪᴅ*
+`
+return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/tZzBS47/image.jpg`},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+
