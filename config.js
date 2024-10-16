122540d71594aee44fc444f3339c4f148693e17c
SHANUWA-MD <shanuwamdv@gmail.com>
2024-10-16 22:20:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+MODE: process.env.MODE || "private",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
+};
