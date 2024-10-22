58a13f55155ffc03b2c0dd2a6b7625abb644ea8d
SHANUWA ID <shanuwatech58@gmail.com>
2024-10-23 00:40:12 +0530

    ❮🙌 DARK ARROW MD 🙌❯

--- plugins/nfsw.js
+++ plugins/nfsw.js
@@ -1,4 +1,4 @@
-😡const config = require('../config');
+const config = require('../config');
 const fs = require('fs');
 const path = require('path');
 const { cmd, commands } = require('../command');
