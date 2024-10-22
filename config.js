5c737f6d587138eee0167be01b644e660389d9c2
MR-CHAMOD <chamodkawidu10@gmail.com>
2024-10-22 22:51:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SWQjURia#q_ucVJMIClGNAFDRmFs4Fz4gkJYVWJD1ySibRqeepF4",
+SESSION_ID: process.env.SESSION_ID || "KXI01LqL#T6MkbMufbvoXSbkXLQuXJUnYvVG0GEJ6uTy8yhbziBw",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
