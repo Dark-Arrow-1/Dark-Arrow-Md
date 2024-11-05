2658d91760b6879843406475191f9b54d7bfeaf0
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-11-06 00:06:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2zwBBZRY#iilEYqCXhmOB8rVEzfM2fY9Fqb7z1BFerCSsr2V4-Qs",
+SESSION_ID: process.env.SESSION_ID || "2zwBBZRY#iilEYqCXhmOB8rVEzfM2fY9Fqb7z1BFerCSsr2V4-Qs",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
